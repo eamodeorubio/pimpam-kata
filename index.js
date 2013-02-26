6ca530e0076bf8e5dfa4d4a33c0844384b29c5e9
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2013-02-26 19:19:59 +0100

    Green

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var expect = require('chai').expect;
 
+
+function pimpam() {
+  return '';
+}
+
 describe('PimPam Game (an extended version of FizzBuzz)', function() {
   context('Not divisible either by 3 or 5 returns empty string', function() {
     it('Number 1', function() {
