955076da6a662e141f08670c39494956f3f35faa
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2013-02-26 19:19:23 +0100

    Not divisible by 3 or 5 returns empty string

---
+++ index.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('PimPam Game (an extended version of FizzBuzz)', function() {
+  context('Not divisible either by 3 or 5 returns empty string', function() {
+    it('Number 1', function() {
+      expect(pimpam(1)).to.be.equal('');
+    });
+  });
+});
