099eb28757c536bd41fe5699a972ed350ddd9f9e
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2013-02-26 20:42:21 +0100

    Refactor. Remove duplication

--- index.js
+++ index.js
@@ -18,23 +18,15 @@ function pimpam(n) {
     };
   }
 
-  var r = [
+  return [
+    divisibleNumberRule({result: 'Pim', divisor: 3}),
+    divisibleNumberRule({result: 'Pam', divisor: 5}),
+    divisibleNumberRule({result: 'Pum', divisor: 2}),
     magicNumberRule({result: 'Toma', magicNumber: 60}),
     magicNumberRule({result: 'Gominolas', magicNumber: 180})
   ].reduce(function(result, rule) {
     return rule(result);
   }, '');
-
-  if(!r) {
-    r = [
-      divisibleNumberRule({result: 'Pim', divisor: 3}),
-      divisibleNumberRule({result: 'Pam', divisor: 5}),
-      divisibleNumberRule({result: 'Pum', divisor: 2})
-    ].reduce(function(result, rule) {
-      return rule(result);
-    }, '');
-  }
-  return r;
 }
 
 describe('PimPam Game (an extended version of FizzBuzz)', function() {
