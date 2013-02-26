fa810ffcd0ae52611892378c9ddcead08a92ca34
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2013-02-26 20:58:46 +0100

    Since we have extracted the rules we add unit tests for them. They are directly green because the rules are already implemented. Note that with the current design we cannot unit test the pimpam function

--- index.js
+++ index.js
@@ -28,6 +28,44 @@ function pimpam(n) {
   }, '');
 }
 
+describe('A MagicNumber rule', function() {
+  context('configured with magic number "33" and result "John"', function() {
+    var rule;
+    beforeEach(function() {
+      rule = magicNumberRule({result: "John", magicNumber: 33});
+    });
+
+    it('when executed with current result "ZZZ" and number 1 will return "ZZZ"', function() {
+      expect(rule('ZZZ', 1)).to.be.equal('ZZZ');
+    });
+
+    it('when executed with current result "ZZZ" and number 33 will return "John"', function() {
+      expect(rule('ZZZ', 33)).to.be.equal('John');
+    });
+  });
+});
+
+describe('A DivisibleNumber rule', function() {
+  context('configured with divisor 7 and result "John"', function() {
+    var rule;
+    beforeEach(function() {
+      rule = divisibleNumberRule({result: "John", divisor: 7});
+    });
+
+    it('when executed with current result "ZZZ" and number 1 will return "ZZZ"', function() {
+      expect(rule('ZZZ', 1)).to.be.equal('ZZZ');
+    });
+
+    it('when executed with current result "ZZZ" and number 7 will return "ZZZJohn"', function() {
+      expect(rule('ZZZ', 7)).to.be.equal('ZZZJohn');
+    });
+
+    it('when executed with current result "XXX" and number 14 will return "XXXJohn"', function() {
+      expect(rule('XXX', 14)).to.be.equal('XXXJohn');
+    });
+  });
+});
+
 describe('PimPam Game (an extended version of FizzBuzz)', function() {
   context('Not divisible either by 3 or 5 returns empty string', function() {
     it('Number 1', function() {
