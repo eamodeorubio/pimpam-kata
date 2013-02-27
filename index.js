e5ccfe0ce3b8c38c8aec1a80979ef72938cff3f0
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2013-02-27 11:36:50 +0100

    Added a test for game. Game can be configured with rules. This time I added more than one spec because we already implemented the game in the pimpam function, so it is only matter of writting tests that describe the implementation that already exists

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var expect = require('chai').expect;
+var chai = require('chai'),
+    expect = chai.expect,
+    sinon = require('sinon');
+
+chai.use(require('sinon-chai'));
 
 function magicNumberRule(opts) {
   return function(resultSoFar, n) {
@@ -71,7 +75,42 @@ describe('A DivisibleNumber rule', function() {
 });
 
 describe('A Game', function() {
+  context('configured with rules X and Y (in that order)', function() {
+    var aGame, ruleX, ruleY;
+    beforeEach(function() {
+      ruleX = sinon.stub();
+      ruleY = sinon.stub();
+      aGame = game([
+        ruleX,
+        ruleY
+      ]);
+    });
 
+    describe('when is executed against number 10', function() {
+      var result, ruleXResult, ruleYResult;
+      beforeEach(function() {
+        ruleXResult = 'XXX';
+        ruleX.returns(ruleXResult);
+        ruleYResult = 'YYY';
+        ruleY.returns(ruleYResult);
+        result = aGame(10);
+      });
+
+      it('will call once rule X with 10 and current result ""', function() {
+        expect(ruleX).to.have.been.calledOnce();
+        expect(ruleX).to.have.been.calledWith('', 10);
+      });
+
+      it('will call once rule Y with 10 and the result of rule X', function() {
+        expect(ruleY).to.have.been.calledOnce();
+        expect(ruleY).to.have.been.calledWith(ruleXResult, 10);
+      });
+
+      it('will return the result of rule Y', function() {
+        expect(result).to.be.equal(ruleYResult);
+      });
+    });
+  });
 });
 
 describe('PimPam is the default game', function() {
